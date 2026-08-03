a3da0cee5cbb20500dec5d7b936f5cefc6ec4bd2
Arina <arinamak02@mail.ru>
2026-08-03 17:40:06 +0300

    refactor_and_added_pause_resume

--- filemodifier.cpp
+++ filemodifier.cpp
@@ -8,25 +8,35 @@ FileModifier::FileModifier(QObject *parent)
     : QObject(parent) {
 }
 
-bool FileModifier::ModifyFile(const QString &input_filepath, const QString &output_filepath) {
+bool FileModifier::ModifyFile(const QString &input_filepath, const QString &output_filepath)
+{
     QFile in_file(input_filepath);
-    if (!in_file.open(QIODevice::ReadOnly)) {
-        emit signalFinished(false, "Cannot open input file: " + input_filepath);
+    QFile out_file(output_filepath);
+    qint64 offset = 0;
+
+    // Если есть сохранённое смещение для этого файла – используем его
+    if (recovery_settings_.curr_input_file_ == input_filepath && recovery_settings_.curr_file_offset_ > 0) {
+        offset = recovery_settings_.curr_file_offset_;
+    }
+
+    QString error_msg;
+    if (!OpenFile(in_file, QIODevice::ReadOnly, input_filepath, offset, error_msg)) {
+        emit signalFinished(false, error_msg);
         return false;
     }
-    QFile out_file(output_filepath);
-    if (!out_file.open(QIODevice::WriteOnly)) {
+
+    if (!OpenFile(out_file, QIODevice::ReadWrite, output_filepath, offset, error_msg)) {
         in_file.close();
-        emit signalFinished(false, "Cannot open output file: " + output_filepath);
+        emit signalFinished(false, error_msg);
         return false;
     }
 
     const qint64 buffer_size = 1024 * 1024;
-    const int key_size = key_.size();
+    const int key_size = settings_.key_.size();
     QByteArray chunk;
 
     while (!in_file.atEnd()) {
-        if (exit_requested_.load()) {
+        if (exit_requested_.load()) {       // ВЫХОД
             out_file.close();
             QFile::remove(output_filepath);
             in_file.close();
@@ -34,9 +44,36 @@ bool FileModifier::ModifyFile(const QString &input_filepath, const QString &outp
             return false;
         }
 
+        if (pause_requested_.load()) {      // ПАУЗА
+            recovery_settings_.curr_file_offset_ = in_file.pos();
+            recovery_settings_.curr_input_file_ = input_filepath;
+            recovery_settings_.curr_output_file_ = output_filepath;
+
+            in_file.close();
+            out_file.close();
+            is_paused_.store(true);
+
+            WaitForResume();    // Ожидаем возобновления
+            if (exit_requested_.load()) {
+                emit signalFinished(false, "Operation cancelled by user");
+                return false;
+            }
+            // После возобновления открываем файлы заново с той же позиции
+            if (!OpenFile(in_file, QIODevice::ReadOnly, input_filepath, offset, error_msg)) {
+                emit signalFinished(false, error_msg);
+                return false;
+            }
+
+            if (!OpenFile(out_file, QIODevice::ReadWrite, output_filepath, offset, error_msg)) {
+                in_file.close();
+                emit signalFinished(false, error_msg);
+                return false;
+            }
+        }
+
         chunk = in_file.read(buffer_size);
         for (int i = 0; i < chunk.size(); ++i) {
-            chunk[i] ^= key_[i % key_size];
+            chunk[i] ^= settings_.key_[i % key_size];
         }
         out_file.write(chunk);
 
@@ -44,6 +81,8 @@ bool FileModifier::ModifyFile(const QString &input_filepath, const QString &outp
         int percent = (bytes_processed_ * 100) / total_bytes_;
         emit signalProgress(percent);
     }
+
+    recovery_settings_.Clear();
 
     in_file.close();
     out_file.close();
@@ -62,30 +101,98 @@ bool FileModifier::IsKeyValid(const QByteArray &key) {
     return true;
 }
 
-void FileModifier::ModifyFiles(const QString &input_path, const QString &output_path, bool modify_filename, const QString &mask, const QByteArray &key, bool remove_source) {
-    if (!IsKeyValid(key)) return;
-    input_path_ = input_path;
-    output_path_ = output_path;
-    modify_filename_ = modify_filename;
-    key_ = key;
-    remove_source_ = remove_source;
-    exit_requested_.store(false);
-
-    filenames_ = GetSuitableFileNames(input_path, mask);
-    bytes_processed_ = 0;
-
-    if(!CalculateTotalBytes()) return;
-
-    for (const QString &filename : filenames_) {
-        if (exit_requested_.load()) return;
-        QString input_file = input_path + "/" + filename;
-        QString output_file = output_path + "/" + GetOutputFilename(output_path, filename, modify_filename_);
-        if(!ModifyFile(input_file, output_file)) return;
+void FileModifier::WaitForResume()
+{
+    QMutexLocker locker(&wait_mutex_);
+    while (pause_requested_.load() && !exit_requested_.load()) {
+        wait_cond_.wait(&wait_mutex_);  //отпускаем мьютекс и блокируем текущий поток до wake
     }
+    is_paused_.store(false);
+}
+
+bool FileModifier::OpenFile(QFile &file, QIODeviceBase::OpenMode mode, const QString &filepath, qint64 offset, QString &error_msg) {
+    if (!file.open(mode)) {
+        error_msg = "Cannot open file: " + filepath;
+        return false;
+    }
+    if (offset > 0) {
+        if (!file.seek(offset)) {
+            error_msg = "Cannot seek in file: " + filepath;
+            file.close();
+            return false;
+        }
+    }
+    return true;
+}
+
+void FileModifier::ModifyFiles(const QString &input_path, const QString &output_path,
+                               bool modify_filename, const QString &mask,
+                               const QByteArray &key, bool remove_source) {
+    if (!IsKeyValid(key)) return;
+
+    settings_.input_path_ = input_path;
+    settings_.output_path_ = output_path;
+    settings_.modify_filename_ = modify_filename;
+    settings_.remove_source_ = remove_source;
+    settings_.key_ = key;
+    settings_.filenames_ = GetSuitableFileNames(input_path, mask);
+
+    exit_requested_.store(false);
+    pause_requested_.store(false);
+    is_paused_.store(false);
+
+    bytes_processed_ = 0;
+    if (!CalculateTotalBytes()) return;
+
+    recovery_settings_.Clear();
+
+    for (int i = recovery_settings_.curr_file_index_; i < settings_.filenames_.size(); ++i) {
+        if (exit_requested_.load()) {
+            emit signalFinished(false, "Operation cancelled by user");
+            return;
+        }
+
+        // Проверка паузы перед началом нового файла
+        if (pause_requested_.load()) {
+            // Сохраняем индекс текущего файла
+            recovery_settings_.curr_file_index_ = i;
+            recovery_settings_.curr_input_file_ = settings_.input_path_ + "/" + settings_.filenames_[i];
+            recovery_settings_.curr_output_file_ = settings_.output_path_ + "/" + GetOutputFilename(settings_.output_path_, settings_.filenames_[i], settings_.modify_filename_);
+            recovery_settings_.curr_file_offset_ = 0; // начало файла
+            is_paused_.store(true);
+            // Ожидаем снятия паузы
+            WaitForResume();
+            if (exit_requested_.load()) {
+                emit signalFinished(false, "Operation cancelled by user");
+                return;
+            }
+        }
+
+        QString filename = settings_.filenames_[i];
+        QString input_file = settings_.input_path_ + "/" + filename;
+        QString output_file = settings_.output_path_ + "/" + GetOutputFilename(settings_.output_path_, filename, settings_.modify_filename_);
+
+        if (!ModifyFile(input_file, output_file)) {
+            return;
+        }
+    }
+
+    emit signalFinished(true, QString());
 }
 
 void FileModifier::slotExitRequested() {
     exit_requested_.store(true);
+}
+
+void FileModifier::slotPauseRequested() {
+    pause_requested_.store(true);
+}
+
+void FileModifier::slotResumeRequested() {
+    pause_requested_.store(false);
+
+    QMutexLocker locker(&wait_mutex_);
+    wait_cond_.wakeOne(); // Будим поток
 }
 
 QStringList FileModifier::GetSuitableFileNames(const QString &in_path, const QString &mask) {
@@ -123,8 +230,9 @@ QStringList FileModifier::GetSuitableFileNames(const QString &in_path, const QSt
 }
 
 bool FileModifier::CalculateTotalBytes() {
-    for (const QString &filename : filenames_) {
-        QString input_file = input_path_ + "/" + filename;
+    total_bytes_ = 0;
+    for (const QString &filename : settings_.filenames_) {
+        QString input_file = settings_.input_path_ + "/" + filename;
         if (!QFileInfo::exists(input_file)) {
             emit signalFinished(false, "File " + input_file + " doesn't exists");
             return false;
--- filemodifier.h
+++ filemodifier.h
@@ -4,6 +4,9 @@
 #include <QString>
 #include <QObject>
 #include <atomic>
+#include <QMutex>
+#include <QWaitCondition>
+#include <QFile>
 
 class FileModifier : public QObject
 {
@@ -18,17 +21,41 @@ signals:
 
 public slots:
     void slotExitRequested();
+    void slotPauseRequested();
+    void slotResumeRequested();
 
 private:
-    QString input_path_;
-    QString output_path_;
-    QStringList filenames_;
-    bool modify_filename_;
-    QByteArray key_;
-    bool remove_source_;
-    std::atomic<bool> exit_requested_{false};
-    qint64 bytes_processed_;
-    qint64 total_bytes_;
+    struct Settings {
+        QString input_path_;
+        QString output_path_;
+        QStringList filenames_;
+        bool modify_filename_ = false;
+        QByteArray key_;
+        bool remove_source_ = false;
+
+        void Clear() {
+            input_path_.clear();
+            output_path_.clear();
+            filenames_.clear();
+            modify_filename_ = false;
+            key_.clear();
+            remove_source_ = false;
+        }
+    };
+
+    struct RecoverySettings {
+        int curr_file_index_{0};
+        qint64 curr_file_offset_{0};
+        QString curr_input_file_;
+        QString curr_output_file_;
+
+        void Clear() {
+            curr_file_index_ = 0;
+            curr_file_offset_ = 0;
+            curr_input_file_.clear();
+            curr_output_file_.clear();
+        }
+    };
 
     bool ModifyFile(const QString &input_filepath, const QString &output_filepath);
     bool IsKeyValid(const QByteArray &key);
@@ -38,6 +65,28 @@ private:
     QString GetOutputFilename(const QString &output_path, const QString &filename) const;
     static QString IncrementNumber(const QString &str);
     static QString GetOutputFilename(const QString &output_path, const QString &filename, bool modify_filename);
+    void WaitForResume();
+
+    static bool OpenFile(QFile& file, QIODevice::OpenMode mode, const QString& filepath, qint64 offset, QString& error_msg);
+
+private:
+    // Настройки для обработки файлов
+    Settings settings_;
+
+    qint64 bytes_processed_;
+    qint64 total_bytes_;
+
+    // Настройки для возобновления обработки с места, на котором остановились
+    RecoverySettings recovery_settings_;
+
+    // Выход / пауза
+    std::atomic<bool> exit_requested_{false};
+    std::atomic<bool> pause_requested_{false};
+    std::atomic<bool> is_paused_{false};
+
+    // Для приостановки потока, чтобы он не нагружал систему
+    QMutex wait_mutex_;               // для синхронизации с QWaitCondition
+    QWaitCondition wait_cond_;
 };
 
 #endif // FILEMODIFIER_H
